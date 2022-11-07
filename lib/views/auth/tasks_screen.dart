5666b7cde539fd54ef4b42aae16d919b51c0f1ab
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-11-07 08:31:39 +0300

    :bug: fixes on time picker

--- lib/views/auth/tasks_screen.dart
+++ lib/views/auth/tasks_screen.dart
@@ -1,5 +1,6 @@
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:taskido/data/models/category_models.dart';
 import 'package:taskido/services/tasks_services.dart';
@@ -43,7 +44,19 @@ class _TasksScreenState extends State<TasksScreen> {
       initialTime: TimeOfDay.now(),
     );
     if (pickedTime != null) {
-      dueDateTextEditingController.text = pickedTime.toString();
+      print("picked ${pickedTime.format(context)}");
+      final now = DateTime.now();
+      DateTime convertedDateTime = DateTime(
+        now.year,
+        now.month,
+        now.day,
+        pickedTime.hour,
+        pickedTime.minute,
+      );
+      print("Converted $convertedDateTime");
+      String formatedTime = DateFormat("HH:mm:ss").format(convertedDateTime);
+      print("FormatedTime $formatedTime");
+      dueTimeTextEditingController.text = formatedTime.toString();
     }
   }
 
@@ -61,6 +74,10 @@ class _TasksScreenState extends State<TasksScreen> {
     if (taskAddFormKey.currentState!.validate()) {
       // Due date is dueDateTextEditingController.text + dueTimeTextEditingController.text in format of "2022-11-06T16:40:42Z",
       // but we need to convert it to "2022-11-06 16:40:42"
+      // 2022-11-10 00:00:00.000
+      // TimeOfDay(10:00)
+      print(dueDateTextEditingController.text);
+      print(dueTimeTextEditingController.text);
       // var dueDate = dueDateTextEditingController.text +
       //     " " +
       //     dueTimeTextEditingController.text;
@@ -171,15 +188,24 @@ class _TasksScreenState extends State<TasksScreen> {
                           value: isImportant,
                           onChanged: (bool? value) {
                             print("Value $value");
-                            print(isImportant);
-                            isImportant = value!;
-                            print(isImportant);
-                            // setState(() {
-                            //   print(isImportant);
-                            //   isImportant = value!;
-                            // });
+                            print("Before $isImportant");
+
+                            setState(() {
+                              // isImportant = value!;
+                            });
+                            print("After: $isImportant");
                           },
                         ),
+
+                        ElevatedButton(
+                          onPressed: taskSubmit,
+                          style:
+                              ElevatedButton.styleFrom(primary: Colors.brown),
+                          child: const Text(
+                            "Add",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
                       ],
                     ),
                   )
@@ -223,122 +249,13 @@ class _TasksScreenState extends State<TasksScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return SimpleDialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  title: const Text(
-                    "Add Task",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20,
-                    ),
-                  ),
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(
-                        left: 20,
-                        right: 20,
-                      ),
-                      child: Column(children: [
-                        Form(
-                          key: taskAddFormKey,
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                controller: taskTaskEditingController,
-                                decoration: const InputDecoration(
-                                  labelText: "Task",
-                                ),
-                                validator: (String? value) {
-                                  if (value == null || value.isEmpty) {
-                                    return "Task is required";
-                                  }
-                                  return null;
-                                },
-                              ),
-                              TextFormField(
-                                controller: noteTextEditingController,
-                                decoration: const InputDecoration(
-                                  labelText: "Note",
-                                ),
-                              ),
-                              TextFormField(
-                                controller: dueDateTextEditingController,
-                                decoration: const InputDecoration(
-                                  icon: Icon(Icons.calendar_today),
-                                  labelText: "Due Date",
-                                ),
-                                readOnly: true,
-                                onTap: () async {
-                                  _selectDate(context);
-                                },
-                                validator: (String? value) {
-                                  if (value == null || value.isEmpty) {
-                                    return "Due Date is required";
-                                  }
-                                  return null;
-                                },
-                              ),
-                              // TextFormField(
-                              //   controller: dueTimeTextEditingController,
-                              //   decoration: const InputDecoration(
-                              //     icon: Icon(Icons.access_time),
-                              //     labelText: "Due Time",
-                              //   ),
-                              //   readOnly: true,
-                              //   onTap: () async {
-                              //     _selectTime(context);
-                              //   },
-                              //   validator: (String? value) {
-                              //     if (value == null || value.isEmpty) {
-                              //       return "Due Time is required";
-                              //     }
-                              //     return null;
-                              //   },
-                              // ),
-                              //important check true and unchecked false
-                              CheckboxListTile(
-                                title: const Text("Important"),
-                                value: isImportant,
-                                onChanged: (bool? value) {
-                                  print("Value $value");
-                                  print("Before $isImportant");
-
-                                  setState(() {
-                                    // isImportant = value!;
-                                  });
-                                  print("After: $isImportant");
-                                },
-                              ),
-
-                              ElevatedButton(
-                                onPressed: taskSubmit,
-                                style: ElevatedButton.styleFrom(
-                                    primary: Colors.brown),
-                                child: const Text(
-                                  "Add",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ]),
-                    )
-                  ],
-                );
-              });
-        },
-
-        //  _addTaskDialog,
+        onPressed: _addTaskDialog,
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+
+
+//
