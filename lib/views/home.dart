f0c9f3939f99909e7b7390b67a61e50ec2e7882e
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-11-10 23:59:07 +0300

    :art: home ui

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    _refresh();
+    // _refresh();
   }
 // set scroll listener
 
@@ -107,19 +107,43 @@ class _HomeScreenState extends State<HomeScreen> {
   //   }
   // }
 
-  void _logoutFnc() async {
-    await authService.logout().then((value) {
-      Navigator.of(context).pop(RouteGenerator.loginPage);
-    });
-  }
+  // void _logoutFnc() async {
+  //   await authService.logout().then((value) {
+  //     Navigator.of(context).pop(RouteGenerator.loginPage);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    // create category map
+    var infor = [
+      {"name": "Home", "icon": Icons.home, "color": Colors.blue, "id": 1},
+      {"name": "Work", "icon": Icons.work, "color": Colors.red, "id": 2},
+      {"name": "School", "icon": Icons.school, "color": Colors.green, "id": 3},
+      {
+        "name": "Shopping",
+        "icon": Icons.shopping_cart,
+        "color": Colors.purple,
+        "id": 4
+      },
+      {
+        "name": "Others",
+        "icon": Icons.more_horiz,
+        "color": Colors.grey,
+        "id": 5
+      },
+      {
+        "name": "Others",
+        "icon": Icons.more_horiz,
+        "color": Colors.grey,
+        "id": 5
+      },
+    ];
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(7, 94, 84, 1),
-        elevation: 10,
+        backgroundColor: Colors.deepOrangeAccent,
+        elevation: 20,
         titleSpacing: 20,
         automaticallyImplyLeading: false,
         title: const Text(
@@ -129,12 +153,350 @@ class _HomeScreenState extends State<HomeScreen> {
             fontSize: 28,
           ),
         ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              showSearch(context: context, delegate: DataSearch());
-            },
-            icon: Icon(Icons.search),
+        // actions: [
+        //   Padding(
+        //     padding: const EdgeInsets.only(
+        //       right: 20,
+        //     ),
+        //     child: IconButton(
+        //       onPressed: () {
+        //         showSearch(context: context, delegate: DataSearch());
+        //       },
+        //       icon: const Icon(Icons.search),
+        //     ),
+        //   )
+        // ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(28),
+          child: Container(
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Categories",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 0,
+                  ),
+                  child: IconButton(
+                    color: Colors.white,
+                    onPressed: () {
+                      showSearch(context: context, delegate: DataSearch());
+                    },
+                    icon: const Icon(Icons.search),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 7,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 7, right: 7),
+            height: 150,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      top: 18,
+                      left: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "27",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 45,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "Tasks",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 3,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Flexible(
+                              child: Text(
+                                "Completed",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                                softWrap: false,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //space the containers
+                const SizedBox(
+                  width: 8,
+                ),
+
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      top: 18,
+                      left: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "17",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 45,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "Tasks",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 3,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Flexible(
+                              child: Text(
+                                "To complete",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                                softWrap: false,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // category to be in a List builder with a row of 2 containers
+          Expanded(
+            child: ListView.builder(
+              // split the list into 2
+              itemCount: (infor.length / 2).ceil(),
+              itemBuilder: (context, index) {
+                int firstIndex = index * 2;
+                int secondIndex = firstIndex + 1;
+                return Container(
+                  height: 180,
+                  margin: const EdgeInsets.only(left: 7, right: 7),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            top: 18,
+                            left: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    infor[firstIndex]["name"].toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  const Text(
+                                    "Tasks",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      infor[firstIndex + 1]["id"].toString(),
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                        // 24,
+                                      ),
+                                      softWrap: false,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            top: 18,
+                            left: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.pink,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    infor[secondIndex]["name"].toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      // 45,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  const Text(
+                                    "Tasks",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      infor[secondIndex + 1]["id"].toString(),
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                      ),
+                                      softWrap: false,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           )
         ],
       ),
